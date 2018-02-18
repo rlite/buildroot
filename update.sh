b67d61652fe3dd9f20e132ad528527bde3522771
Vincenzo Maffione <v.maffione@gmail.com>
2018-02-18 11:28:45 +0100

    ouroboros: improve scripts and names

--- update.sh
+++ update.sh
@@ -1,36 +1,33 @@
 #!/bin/bash
 
-# This script updates the rlite image and copy it to
+# This script updates the ouroboros image and copy it to
 # the demo/buildroot directory in your repository.
 #
-# By default, rlite code to include in the image is
+# By default, ouroboros code to include in the image is
 # taken from github. If you want to use the code of
-# your local git tree, open package/rlite/rlite.mk
+# your local git tree, open package/ouroboros/ouroboros.mk
 # and use the following configuration:
 #
-#   RLITE_SITE_METHOD = local
-#   RLITE_SITE = /path/to/your/rlite/repo/
+#   OUROBOROS_SITE_METHOD = local
+#   OUROBOROS_SITE = /path/to/your/ouroboros/repo/
 
 if [[ ! -f .config ]]; then
-    cp RLITE_BUILDROOT_CONFIG .config
-    echo "Imported default .config for rlite images"
+    cp OUROBOROS_BUILDROOT_CONFIG .config
+    echo "Imported default .config for ouroboros images"
 fi
 
-diff -pu .config RLITE_BUILDROOT_CONFIG > /dev/null
+diff -pu .config OUROBOROS_BUILDROOT_CONFIG > /dev/null
 if [ "$?" != "0" ]; then
-    echo "Warning: you are using a .config different from RLITE_BUILDROOT_CONFIG"
+    echo "Warning: you are using a .config different from OUROBOROS_BUILDROOT_CONFIG"
     echo "  I assume you know what you are doing ..."
 fi
 
 set -x
 
-rm -rf dl/rlite-master.tar.gz
-make rlite-dirclean
+rm -rf dl/ouroboros-master.tar.gz
+make ouroboros-dirclean
 # This usually requires temporarily using local SITE_METHOD
 make
 
-# Copy the generated image to the demo/buildroot directory
-# in your rlite git tree.
-# Please change the destination directory with the path of
-# your own rlite tree.
-cp output/images/* ~/git/rlite/demo/buildroot/
+# Please change the destination directory with a path of your choice.
+cp output/images/* /tmp/
